d1d19d885374015dcc77f12c49ef79b60f0cec6a
priyanshuRj <priyanshurajput0071109@gmail.com>
2023-03-28 22:10:48 +0530

    Managed Settings

--- classwoodBackend/classwoodBackend/settings.py
+++ classwoodBackend/classwoodBackend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEVELOPMENT")
 
-ALLOWED_HOSTS = ['pandu420.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['pandu420.pythonanywhere.com','127.0.0.1', 'localhost']
 
 
 # Application definition
